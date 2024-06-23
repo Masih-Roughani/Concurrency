ae630fc32592787cf3881d2ddacd27f5f117fde5
Masih Roughani <163646001+Masih-Roughani@users.noreply.github.com>
2024-06-23 14:00:23 +0330

    Third exercise finished

--- third/src/Main.java
+++ third/src/Main.java
@@ -1,5 +1,62 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        AtomicInteger success = new AtomicInteger(0);
+        AtomicInteger unSuccess = new AtomicInteger(0);
+
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            if (getRandomBoolean()){
+                success.incrementAndGet();
+            }else{
+                unSuccess.incrementAndGet();
+            }
+        };
+        
+        /*
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(25);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+                if (getRandomBoolean()){
+                    success.incrementAndGet();
+                }else{
+                    unSuccess.incrementAndGet();
+                }
+            }
+        };
+        Can be replaced.
+        */
+
+        ExecutorService executorService = Executors.newFixedThreadPool(25);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(runnable);
+        }
+
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(110, TimeUnit.MILLISECONDS);
+            System.out.println("Successful requests : " + success.get());
+            System.out.print("Unsuccessful requests : " + unSuccess.get());
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public static boolean getRandomBoolean() {
+        return Math.random() < 0.5;
     }
 }
