228eabfbfb7d12455330dc677e7f5990092f68eb
Masih Roughani <163646001+Masih-Roughani@users.noreply.github.com>
2024-06-23 15:14:42 +0330

    Fourth exercise finished

--- fourth/src/Main.java
+++ fourth/src/Main.java
@@ -1,5 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        List<Callable<Integer>> tasks = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            tasks.add(() -> {
+                Thread.sleep(1000);
+                int num = (((finalI * 2) + 3) * 2 + 4) + 10;
+                return (int) (Math.random() * num);
+            });
+        }
+
+        List<Future<Integer>> doneTasks = pool.invokeAll(tasks);
+
+        for (Future<Integer> doneTask : doneTasks) {
+            if (doneTask.isDone() && !doneTask.isCancelled()) {
+                System.out.println("Result : " + doneTask.get());
+            } else {
+                System.out.println("Task has been cancelled or has been done.");
+            }
+        }
+
+        pool.shutdown();
     }
 }
